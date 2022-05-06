3765767f9dc5d3a3e4c8a10051ca80970f607561
Marko Kordic <marko.kordic1996@gmail.com>
2022-05-06 23:25:07 +0200

    .

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String eyeColor = "Blue";
+        System.out.println(eyeColor);
+        int myWeight = 83;
+                System.out.println(myWeight);
+        char firstWord = 'A';
+        System.out.println(firstWord);
+        double whatTime = 1.35;
+        System.out.println(whatTime);
+        boolean haveBrother = true;
+        System.out.println(haveBrother);
+        String dogName = "Rocky";
+        System.out.println(dogName);
+    }
+}
