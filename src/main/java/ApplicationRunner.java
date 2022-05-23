231cd84e5b32525e9b21fdd6caba8a1f5be6a561
Marko Kordic <marko.kordic1996@gmail.com>
2022-05-23 21:32:08 +0200

    Application.java

---
+++ src/main/java/ApplicationRunner.java
@@ -0,0 +1,7 @@
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        Application person = new Application("Adam",40.5, 178);
+        person.checkPerson();
+    }
+}
+
