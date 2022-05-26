ea8d6676c6c7ac26482fbc1f4cac4c80871d50a8
Marko Kordic <marko.kordic1996@gmail.com>
2022-05-26 20:02:56 +0200

    User.java

---
+++ src/main/java/User.java
@@ -0,0 +1,35 @@
+public class User {
+   String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    String getName() {
+        return this.name;
+
+    }
+
+    int getAge() {
+        return this.age;
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
