7e7d06df2592dc1517211be3927a2ac536925a1f
Marko Kordic <marko.kordic1996@gmail.com>
2022-08-04 12:21:17 +0200

    Modul 10.4 java

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
@@ -0,0 +1,52 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+
+class BookControllerTestSuite {
+
+    @Test
+    void shouldFetchBooks() {
+        //given
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(new BookDto("Title 1", "Author 1"));
+        booksList.add(new BookDto("Title 2", "Author 2"));
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+
+        //when
+        List<BookDto> result = bookController.getBooks();
+
+        //then
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void shouldAddBook() {
+        //given
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        BookDto bookDto = Mockito.mock(BookDto.class);
+        List<BookDto> bookList = new ArrayList<>();
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(bookList);
+
+        //when
+        bookList.add(bookDto);
+        bookList.add(bookDto);
+
+        //then
+        assertThat(bookController.getBooks()).hasSize(2);
+
+
+    }
+}
