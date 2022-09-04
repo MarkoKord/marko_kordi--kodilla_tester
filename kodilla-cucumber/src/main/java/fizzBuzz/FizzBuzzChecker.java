9cf771dedb98e2030f8bbc2fa50d4f4363a1174b
Marko Kordic <marko.kordic1996@gmail.com>
2022-09-04 10:58:55 +0200

    modul 14.2

---
+++ kodilla-cucumber/src/main/java/fizzBuzz/FizzBuzzChecker.java
@@ -0,0 +1,16 @@
+package fizzBuzz;
+
+public class FizzBuzzChecker {
+
+    public String fizzBuzzChecker(int number, int numberA, int numberB) {
+        if ((number % numberA == 0) && (number % numberB == 0)) {
+            return "FizzBuzz";
+        } else if ((number % numberA == 0) && (number % numberB != 0)) {
+            return "Fizz";
+        } else if ((number % numberA != 0) && (number % numberB == 0)) {
+            return "Buzz";
+        } else return "None";
+    }
+}
+
+
---
+++ kodilla-cucumber/src/test/java/fizzBuzz/FizzBuzzSteps.java
@@ -0,0 +1,25 @@
+package fizzBuzz;
+
+
+import io.cucumber.java8.En;
+import org.junit.Assert;
+
+public class FizzBuzzSteps implements En {
+        private int number;
+        private String answer;
+
+        public FizzBuzzSteps() {
+
+            Given("^Number is equal to (.*)$", (String number) ->
+                    this.number = Integer.parseInt(number));
+
+            When("^Check if the number is divisible by (.*) and (.*)$", (String numberA, String numberB) -> {
+                FizzBuzzChecker fizzBuzzChecker = new FizzBuzzChecker();
+                this.answer = fizzBuzzChecker.fizzBuzzChecker(this.number, Integer.parseInt(numberA), Integer.parseInt(numberB));
+            });
+
+            Then("Should get an answer which is: {string}", (String string) ->
+                    Assert.assertEquals(string, this.answer));
+        }
+    }
+
